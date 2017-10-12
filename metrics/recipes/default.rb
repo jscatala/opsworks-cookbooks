a9ebc185f76ae306eaa9e727e61c51dc01ae55a4
Juan Catalan <jcatalanolmos@gmail.com>
2017-10-12 17:52:25 -0300

    metrics recipes sync with production configuration

---
+++ metrics/recipes/default.rb
@@ -0,0 +1,35 @@
+#
+# Cookbook:: metrics
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+
+
+directory '/opt/cloudmonitor' do
+    owner 'root'
+    group 'root'
+    mode '0644'
+    action :create
+end
+
+
+bash 'virtualenv and cloudwatchmon' do
+    code <<-EOH
+    pip install --upgrade pip
+    pip install virtualenv
+    virtualenv /opt/cloudmonitor/env
+    /opt/cloudmonitor/env/bin/pip install cloudwatchmon
+    EOH
+end
+
+
+git 'cloudmonitor' do 
+    repository 'https://github.com/osiegmar/cloudwatch-mon-scripts-python.git'
+    destination '/opt/cloudmonitor/cloudwatch-mon-scripts-python'
+end
+
+
+cron 'cloudwatchmetrics' do
+    minute '*/5'
+    command "/opt/cloudmonitor/env/bin/python /opt/cloudmonitor/cloudwatch-mon-scripts-python/cloudwatchmon/cli/put_instance_stats.py --mem-util --disk-space-util --disk-path=/"
+end
